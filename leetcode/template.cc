735988c8ab8b0e98fe3fca76e539e5fe36553ef3
Felix Cuello <felix.cuello@gmail.com>
2023-01-07 09:56:41 -0300

    Added templates for leetcode

---
+++ leetcode/template.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+#define UL unsigned long
+#define ULL unsigned long long
+#define LL long long
+
+#define BEGZ(x) __builtin_clz(x)      // Return the number of zeroes at the beginning of the bit representation
+#define ENDZ(x) __builtin_ctz(x)      // Return the number of zeroes at the end of the bit representation
+#define ONES(x) __builtin_popcount(x) // Return the number of ones in the bit representation
+#define PARITY(x) __builtin_parity(x) // Return the parity of the numbers of one in the bit representation
+#define FOR(a,b) for(unsigned long i=a; i<b; i++)
+#define DEBUG true
+
+using namespace std;
+
+int main() {
+        cin.sync_with_stdio(false);
+        Solution* sol = new Solution();
+        sol->
+        return 0;
+}
+
